ad2ff2a69b94240bad366eb93a699fe69d9ab52f
srividhya32 <116020079+srividhya32@users.noreply.github.com>
2024-08-07 20:41:11 +0530

    Update Scenario1.sql

--- Week-2/PlSql/1.Control_Structures/Scenario1.sql
+++ Week-2/PlSql/1.Control_Structures/Scenario1.sql
@@ -1,1 +1,15 @@
+DECLARE
+    CURSOR c_customers IS
+        SELECT c.CustomerID, l.LoanID, l.InterestRate
+        FROM Customers c
+        JOIN Loans l ON c.CustomerID = l.CustomerID
+        WHERE TRUNC((SYSDATE - c.DOB) / 365) > 60; -- Simplified age calculation
+BEGIN
+    FOR rec IN c_customers LOOP
+        UPDATE Loans
+        SET InterestRate = rec.InterestRate * 0.99 -- Applying 1% discount
+        WHERE LoanID = rec.LoanID;
+    END LOOP;
+    COMMIT;
+END;
 
