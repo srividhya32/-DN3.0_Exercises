811b7f9a77c6bb180a5ad4de4e36845996567b0b
srividhya32 <116020079+srividhya32@users.noreply.github.com>
2024-08-08 13:02:02 +0530

    Delete Week-2/plSql directory

--- Week-2/plSql/Triggers/Scenario1.sql.sql
+++
@@ -1,8 +0,0 @@
-SQL> CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
-  2  BEFORE UPDATE ON Customers
-  3  FOR EACH ROW
-  4  BEGIN
-  5      :NEW.LastModified := SYSDATE;
-  6  END;
-  7  /
-
