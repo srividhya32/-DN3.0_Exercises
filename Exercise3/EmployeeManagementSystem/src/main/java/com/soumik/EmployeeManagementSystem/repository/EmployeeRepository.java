207ebc39179a325665050872dafca39100e7025b
srividhya32 <116020079+srividhya32@users.noreply.github.com>
2024-08-15 07:48:22 +0530

    Delete Exercise3/EmployeeManagementSystem directory

--- Exercise3/EmployeeManagementSystem/src/main/java/com/soumik/EmployeeManagementSystem/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.soumik.EmployeeManagementSystem.repository;
-
-
-import main.java.com.soumik.EmployeeManagementSystem.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
